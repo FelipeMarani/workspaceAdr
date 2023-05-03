3124678e15f6d8499e973a79196bdc7bfbb77a48
FelipeMarani <felipejesusdasilva02@gmail.com>
2023-05-03 09:11:18 -0300

    primeiro commit

---
+++ miniprojectAdr/src/main/java/br/com/itb/miniprojectAdr/MiniprojectAdrApplication.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojectAdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectAdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectAdrApplication.class, args);
+	}
+
+}
