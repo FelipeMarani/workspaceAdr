43264993e8da800de565480d267d1eb5fd643e27
FelipeMarani <felipejesusdasilva02@gmail.com>
2023-05-31 10:57:34 -0300

    novo

---
+++ Adrprojeto/src/main/java/com/miniprojectoAdr/Adrprojeto/AdrprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.miniprojectoAdr.Adrprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrprojetoApplication.class, args);
+	}
+
+}
---
+++ Adrprojeto/src/test/java/com/miniprojectoAdr/Adrprojeto/AdrprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniprojectoAdr.Adrprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
